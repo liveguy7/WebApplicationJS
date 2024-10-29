2ac85f39f95b471042a4472904d8cf180e8af935
jerom <jerom@LAPTOP-L0BU5SNG>
2024-10-28 21:19:26 -0400

    js

--- WebApplicationJS/wwwroot/js/site.js
+++ WebApplicationJS/wwwroot/js/site.js
@@ -1,4 +1,10 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+$(function () {
+    $("p").css("background-color", "yellow");
+});
 
-// Write your JavaScript code.
+$(function () {
+    $("h1").css("background-color", "blue");
+})
+
+
